fa07b3238249ff5b62441918416433afea067f68
Guilherme Lima <guilhermelimaruby@gmail.com>
2019-06-12 13:40:31 -0300

    video2.2

--- main.go
+++ main.go
@@ -5,12 +5,26 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float32
+	Quantidade int
+}
+
 var tmpl = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", Index)
 	http.ListenAndServe(":8000", nil)
 }
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "Index", nil)
+	dados := []Produto{
+		{Nome: "Camiseta", Descricao: "Muito bonita", Preco: 39.9, Quantidade: 10},
+		{Nome: "Sapato", Descricao: "Para ocasiões formais", Preco: 199.99, Quantidade: 5},
+		{Nome: "Notebook", Descricao: "Muito rápido", Preco: 2099.99, Quantidade: 1},
+		{Nome: "Fone", Descricao: "Confortável e boa sonoridade", Preco: 19.99, Quantidade: 10},
+	}
+	tmpl.ExecuteTemplate(w, "Index", dados)
 }
